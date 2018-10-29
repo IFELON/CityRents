41f738a4630151b99a7f8b039e61cd36438eb85e
acer <reddynani5566>
2018-10-29 17:37:37 +0530

    check my splash screen

---
+++ app/src/main/java/in/vaaraahi/cityrents/Imageslider.java
@@ -0,0 +1,17 @@
+package in.vaaraahi.cityrents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class Imageslider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.imageslider);
+
+
+    }
+}
